8c509a38070c9713e169c48d0d85de0c5f833952
Alok <alok@cognicept.systems>
2020-02-15 02:06:18 +0530

    web-joypad control

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/devcs/Desktop/rosbridge_template/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/devcs/catkin_ws6/devel;/home/devcs/catkin_ws_gmapping/devel;/home/devcs/catkin_ws_openslam_gmapping/devel;/home/devcs/catkin_ws_turtleBot/devel;/opt/ros/melodic".split(';') if "/home/devcs/catkin_ws6/devel;/home/devcs/catkin_ws_gmapping/devel;/home/devcs/catkin_ws_openslam_gmapping/devel;/home/devcs/catkin_ws_turtleBot/devel;/opt/ros/melodic" != "" else []
---
+++ build/robot_gui_bridge/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/devcs/Desktop/rosbridge_template/src/robot_gui_bridge
+# Build directory: /home/devcs/Desktop/rosbridge_template/build/robot_gui_bridge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
